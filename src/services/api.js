bb75af8e1fec34aa42bead41cf70a6b8d6f67414
SantiagoZ98 <dog.lion2298@outlook.com>
2026-06-04 13:55:21 -0500

    feat: :fire: New changes in the environments

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-    baseURL: import.meta.env.VITE_API_URL || "/api",
+    baseURL: process.env.REACT_APP_API_URL || "https://mi-api-backend.railway.app/api",
     timeout: 30000,
     headers: {
         "Content-Type":"application/json",
